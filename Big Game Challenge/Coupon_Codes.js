6f7213f418e856d437c2c413a46f72352ca6be5c
Rob <lum.robert@gmail.com>
2014-01-15 12:10:38 -0500

    removed logs

--- Big Game Challenge/Coupon_Codes.js
+++ Big Game Challenge/Coupon_Codes.js
@@ -5,7 +5,6 @@ jQuery(function($){
 	var validate = function (){
 		var newValue = JSON.stringify($('#actual_code').text());
 		if (blankValue !== newValue){
-			console.log(blankValue + '' + newValue)
 			displayMessage(newValue);
 		} else {
 			setTimeout(validate, 1000);
@@ -13,7 +12,6 @@ jQuery(function($){
 	}
 
 		var displayMessage = function (displayText){
-			console.log(displayText);
 			var messageCase;
 			switch(displayText){
 				case "\"50GC\"" :
@@ -45,7 +43,6 @@ jQuery(function($){
 				break;
 
 			}
-			console.log(displayText);
 			$(messageCase).appendTo('#actual_code');
 			$(messageCase).css('display','block');
 		}
