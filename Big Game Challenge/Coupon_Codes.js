a0255e5f3427382279a6f72de4e40f2ec298d367
Rob <lum.robert@gmail.com>
2014-01-15 16:10:28 -0500

    Added winner share copies

--- Big Game Challenge/Coupon_Codes.js
+++ Big Game Challenge/Coupon_Codes.js
@@ -1,7 +1,9 @@
 jQuery(function($){
 
-	var blankValue = JSON.stringify($('#actual_code').text());
+	var bitLy = "http://bit.ly/1bTTLUT"
+		,blankValue = JSON.stringify($('#actual_code').text());
 
+	//Check for coupon code
 	var validate = function (){
 		var newValue = JSON.stringify($('#actual_code').text());
 		if (blankValue !== newValue){
@@ -11,6 +13,7 @@ jQuery(function($){
 		}
 	}
 
+		//Display Win or Loss Message
 		var displayMessage = function (displayText){
 			var messageCase;
 			switch(displayText){
@@ -43,8 +46,7 @@ jQuery(function($){
 				break;
 
 			}
-			var prize = $(messageCase);
-			$('#actual_code').html(prize);		
+			$(messageCase).appendTo('#view_code');	
 			$(messageCase).css('display','block');
 		}
 
@@ -52,6 +54,22 @@ jQuery(function($){
 		setTimeout(validate, 1000);
 	});
 
+	//Facebook Share Copy
+	$('<img id="facebookShare" src="https://opop.cachefly.net/RobLum/Misc/facebook_share_bgc_011314.png">').appendTo('.winMessage');
+			
+			$('#facebookShare').click(function(){
+				FB.ui({
+					method: 'feed',
+					link: bitLy,
+					caption: 'I just took Unilever\'s Big Game Challenge and INSTANTLY won a prize! I\'m also entered to win a 55" TV. Take the challenge for yourself and enter for a chance to win big!',
+					description: 'Unilever\'s Big Game Challenge - Pick your favorites in the Big Game Challenge and you could INSTANTLY win one of hundreds of prizes. You\'ll also be entered to win the grand prize: a 55" flat screen TV with surround sound!',
+				}, function(response){});
 
+			});
+
+	//Twitter Share Copy
+	$('<a target="_blank" href="http://twitter.com/share?text=I%20just%20took%20%40unileverusa%E2%80%99s%20Big%20Game%20Challenge%2C%20INSTANTLY%20won%20a%20prize%20%26%20entered%20to%20win%20a%2055%22%20TV.%20Take%20the%20Challenge%3A&url=' + bitLy + '"><img src="https://opop.cachefly.net/RobLum/Misc/twitter_share_bgc_011314.png"></a>').appendTo('.winMessage');
+			
 	
 });
+
