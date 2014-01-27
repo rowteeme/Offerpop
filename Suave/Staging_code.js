1c5abba418665293ed36d2b6c9f7a8a437eecd07
Rob <lum.robert@gmail.com>
2014-01-27 12:53:02 -0500

    Contains Production Quiz Choices

--- Suave/Staging_code.js
+++ Suave/Staging_code.js
@@ -2,30 +2,47 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
 <script src="https://opop.cachefly.net/Rotimi/jquery.ui.touch-punch.min.js"></script>
+<script src="https://opop.cachefly.net/RobLum/Suave/Form_Validation_v3.js"></script>
 
 <script>
 jQuery(document).ready(function($) {
 
 		//Insert Form Instruction Text
-		$('<div id="formInstructions">Complete the entry form below for a chance to win a coupon for a full-sized Suave Professionals® Natural Infusion product</div>').prependTo('.SBox');
+		$('<div id="formInstructions">Complete the entry form below for a chance to win a coupon for a full-sized Suave Professionals<sup>®</sup> Natural Infusion product</div>').prependTo('.SBox');
 
 		//Include Privacy Text in sign up form
-		$('<div id="privacyText">Helping you with your everyday needs is important to us. Therefore, from time to time, we may wish to send you information, samples or special offers that we feel may be of interest to you regarding Suave®, or other complementary brands from Unilever or other carefully selected companies. If you would rather not opt-in to receive such information, please uncheck the box below. For more information or to remove yourself from future contact, please visit our Privacy Policy.</div>').insertBefore('#sfield_optin1');
+		$('<div id="privacyText">Helping you with your everyday needs is important to us. Therefore, from time to time, we may wish to send you information, samples or special offers that we feel may be of interest to you regarding Suave<sup>®</sup>, or other complementary brands from Unilever or other carefully selected companies. If you would rather not opt-in to receive such information, please uncheck the box below. For more information or to remove yourself from future contact, please visit our <a href="http://www.unileverprivacypolicy.com/en_us/policy.aspx" target="_blank">Privacy Policy</a>.</div>').insertBefore('#sfield_optin1');
 
 		//Replace Share copy on Thank you page
 		$('#QuizShareAfterSubmission').html('<a onclick="popShare();return false;" class="requestbutton" title="Share with Friends"><img src="https://s3.amazonaws.com/com.offerpop.datastore/438279/urF3KK.png"></a>');
 
 		//Create the Dropbox container div for the five Dropboxes
-		$('#question_choice_87674').after('<div id="dropbox"><div class="QuizChoice"></div></div>');
+		$('#question_choice_89699').after('<div id="dropbox"><div class="QuizChoice"></div></div>');
 
 		//Create the bottom section and its buttons
-		$('#dropbox').append('<div id="bottom"><a href="#" target="_self"><div id="reset"></div></a><a href="javascript:void(0);"><div class="next"></div></a><a href="javascript:void(0);"><div id="skip"></div></a></div>');
+		$('#dropbox').append('<div id="bottom"><a href="http://opop.cachefly.net/Suave%20Whats%20Your%20Hair%20Resolution%20SS%20FINAL_Final.pdf" target="_blank"><div id="reset">OFFICIAL RULES</div></a><a href="javascript:void(0);"><div class="next"></div></a><a href="javascript:void(0);"><div id="skip"></div></a></div>');
 
-		$('<div id="thankYouText">Visit <a href="http://www.suave.com/" target="_blank">Suave.com</a> for more information on the NEW Suave Professionals® Natural Infusion collection.</div>').appendTo('#QuizShareAfterSubmission');
+		$('<div id="thankYouText">Visit <a href="http://www.suave.com/" target="_blank">Suave.com</a> for more information on the NEW Suave Professionals<sup>®</sup> Natural Infusion collection.</div>').appendTo('#QuizShareAfterSubmission');
 
+		$('<div id="officialRules">No purchase necessary. Void where prohibited. Suave Professionals<sup>®</sup> Natural Infusion Sweepstakes is sponsored by Conopco, Inc., d/b/a Unilever. Open to legal residents of the 50 U.S. & D.C., 18 or older. Begins 1/27/14 at 10:00 a.m. ET & ends 2/12/14 at 11:59 p.m. ET. For official rules, click <a href="http://opop.cachefly.net/Suave%20Whats%20Your%20Hair%20Resolution%20SS%20FINAL_Final.pdf" target="_blank">here</a>.</div>').insertAfter('.CFooterDivider');
+
+		$('#sfield_gender').insertBefore('#sfield_city');
+
+		//Unilever Opt-ins (Mutually Exclusive)
 		$('#optin1').prop('checked', true);
-		$('#optin2').prop('checked', true);
-		$('#optin3').prop('checked', true);
+		try { $('#optin1').checkboxradio("refresh"); } catch(e) { }
+
+		$('#optin1').change(function(event) {
+       		$('#optin2').prop('checked', false);
+			try { $('#optin2').checkboxradio("refresh"); } catch(e) { }
+   		});
+   
+   		$('#optin2').change(function(event) {
+       		$('#optin1').prop('checked', false);
+			try { $('#optin1').checkboxradio("refresh"); } catch(e) { }
+   		});
+		//$('#optin2').prop('checked', true);
+		//$('#optin3').prop('checked', true);
 
 		//Droppable function that assigns a drop event to any dropbox ID taken as a parameter.
 		function _droppable_(dropbox_element_id) {
@@ -74,10 +91,10 @@ jQuery(document).ready(function($) {
 		});
 
 		//Create variables for each Quiz choice. 
-		qc1 = 'choice_87671';
-		qc2 = 'choice_87672';
-		qc3 = 'choice_87673';
-		qc4 = 'choice_87674';
+		qc1 = 'choice_89696';
+		qc2 = 'choice_89697';
+		qc3 = 'choice_89698';
+		qc4 = 'choice_89699';
 
 		//Applies our droppable function to all the dropboxes. 
 		_droppable_('#dropbox1');
@@ -117,6 +134,7 @@ jQuery(document).ready(function($) {
 	});
 
 </script>
+<![if !IE]>
 <script>
 jQuery(function($){
 	//Placeholders for Submit Form
@@ -125,9 +143,8 @@ jQuery(function($){
 	$("#lastname").attr('placeholder', 'Last name');
 	$("#address").attr('placeholder', 'Street address');
 	$('#city').attr('placeholder', 'City');
-	$('#state').attr('placeholder', 'State');
 	$('#zip').attr('placeholder', 'Zip code');
-	$("#birthday").attr('placeholder', 'Birthday');
+	$("#birthday").attr('placeholder', 'Date of Birth');
 	//Fix Placeholders
 	$('[placeholder]').parents('form').submit(function() {
 	  $(this).find('[placeholder]').each(function() {
@@ -152,4 +169,5 @@ jQuery(function($){
 	}).blur();
 });
 </script>
+<![endif]>
 <style>
