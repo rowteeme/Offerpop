b9e505d372adf0b0960a396a8e659f9119f4f239
Rotimi Oyewole <Rotimi.Oyewole@gmail.com>
2014-04-16 18:10:20 -0400

    Matrix folder organization + english
    Getting started on english version.

---
+++ Matrix/code/english/desktop.js
@@ -0,0 +1,20 @@
+<script>
+
+jQuery(function($) {
+
+    var legal = $('.legal');
+
+    // Insert custom submit button
+    $('.SSubmit').append('<button class="uploadable" id="custom-submit" type="submit" value="Submit"></button>');
+
+    $('#sfield_email').insertAfter('#sfield_zip');
+    $('#sfield_birthday').insertAfter('#sfield_lastname');
+
+    $(legal).insertAfter('.SSubmit');
+
+    if ($('#After').length > 0) {
+        $('#After').css('background','url("https://opop.cachefly.net/Matrix/bg-aftersignup-french.png")');
+    }
+});
+
+</script>
