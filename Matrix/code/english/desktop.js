bf9514796512372c5922b7f1a3356fe49cff014f
Rotimi Oyewole <Rotimi.Oyewole@gmail.com>
2014-04-17 11:23:33 -0400

    Wrapping up english
    Wrapping up english version, set up alternate state for already signed
    up users

--- Matrix/code/english/desktop.js
+++ Matrix/code/english/desktop.js
@@ -13,8 +13,18 @@ jQuery(function($) {
     $(legal).insertAfter('.SSubmit');
 
     if ($('#After').length > 0) {
-        $('#After').css('background','url("https://opop.cachefly.net/Matrix/bg-aftersignup-english.png")');
+        $('#After').css('background','url("https://opop.cachefly.net/Matrix/bg-aftersignup-english2.png")');
     }
-});
 
+    $('.fb-button').click(function(e){
+        e.preventDefault();
+        FB.ui({
+            method: 'feed',
+            name: 'Matrix Canada',
+            link: 'http://bit.ly/QeQ8SA',
+            caption: 'http://bit.ly/QeQ8SA',
+            description: 'I just claimed my free MatrixCanada Biolage samples. Claim yours too: http://bit.ly/QeQ8SA'
+        });
+    });
+});
 </script>
